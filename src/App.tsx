0ec68207bcb7566379da0ec64c0354c608247094
Danny Meuse <danny.meuse@gmail.com>
2022-01-07 18:57:42 -0800

    Changed some state logic, trying to get Github Pages working

--- src/App.tsx
+++ src/App.tsx
@@ -14,78 +14,140 @@ export const ACTIONS = {
 function reducer(state: any, { type, payload }: any) {
   switch (type) {
     case ACTIONS.ADD_DIGIT:
+      if (state.overwrite) {
+        return {
+          ...state,
+          currentOperand: payload.digit,
+          overwrite: false,
+        };
+      }
+      if (payload.digit === "0" && state.currentOperand === "0") return state;
+
+      if (payload.digit === "." && state.currentOperand == null) {
+        return state;
+      }
+      if (payload.digit === "." && state.currentOperand.includes(".")) {
+        return state;
+      }
+      if (payload.digit === "." && state.currentOperand.includes("."))
+        return state;
+
       return {
         ...state,
         currentOperand: `${state.currentOperand || ""}${payload.digit}`,
       };
     case ACTIONS.DELETE_DIGIT:
+      if (state.overwrite) {
+        return {
+          ...state,
+          overwrite: false,
+          currentOperand: null,
+        };
+      }
+
+      if (state.currentOperand == null) return state;
+      if (state.currentOperand.length === 1) {
+        return { ...state, currentOperand: null };
+      }
       return {
         ...state,
-        currentOperand: `${
-          state.currentOperand.slice(0, state.currentOperand.length - 1) || ""
-        }`,
+        currentOperand: state.currentOperand.slice(0, -1),
       };
     case ACTIONS.CLEAR:
       return {
-        currentOperand: "",
-        previousOperand: "",
-        operation: "",
+        currentOperand: "0",
       };
     case ACTIONS.CHOOSE_OPERATION:
+      if (state.currentOperand == null && state.previousOperand == null)
+        return state;
+
+      if (state.currentOperand == null) {
+        return {
+          ...state,
+          operation: payload.operation,
+        };
+      }
+
+      if (state.previousOperand == null) {
+        return {
+          ...state,
+          operation: payload.operation,
+          previousOperand: state.currentOperand,
+          currentOperand: null,
+        };
+      }
+
       return {
-        currentOperand: "",
-        previousOperand: `${state.currentOperand}`,
-        operation: `${payload.operation}`,
+        ...state,
+        previousOperand: evaluate(state),
+        operation: payload.operation,
+        currentOperand: null,
       };
     case ACTIONS.EVALUATE:
-      if (state.operation === "-")
-        return {
-          currentOperand: `${
-            Number(state.previousOperand) - Number(state.currentOperand)
-          }`,
-          previousOperand: `${state.previousOperand} - ${state.currentOperand}`,
-          operation: "",
-        };
-      if (state.operation === "+")
-        return {
-          currentOperand: `${
-            Number(state.previousOperand) + Number(state.currentOperand)
-          }`,
-          previousOperand: `${state.previousOperand} + ${state.currentOperand}`,
-          operation: "",
-        };
-      if (state.operation === "÷")
-        return {
-          currentOperand: `${
-            Number(state.previousOperand) / Number(state.currentOperand)
-          }`,
-          previousOperand: `${state.previousOperand} ÷ ${state.currentOperand}`,
-          operation: "",
-        };
+      if (
+        state.operation == null ||
+        state.currentOperand == null ||
+        state.previousOperand == null
+      ) {
+        return state;
+      }
 
-      if (state.operation === "×")
-        return {
-          currentOperand: `${
-            Number(state.previousOperand) * Number(state.currentOperand)
-          }`,
-          previousOperand: `${state.previousOperand} × ${state.currentOperand}`,
-          operation: "",
-        };
-      else
-        return {
-          currentOperand: state.currentOperand,
-          previousOperand: state.previousOperand,
-          operation: state.operation,
-        };
+      return {
+        ...state,
+        overwrite: true,
+        previousOperand: null,
+        operation: null,
+        currentOperand: evaluate(state),
+      };
+
     default:
       return "Provide action type";
   }
 }
 
+function evaluate({ currentOperand, previousOperand, operation }: any) {
+  const prev = parseFloat(previousOperand);
+  const current = parseFloat(currentOperand);
+
+  if (isNaN(prev) || isNaN(current)) return "";
+  let computation: any = "";
+
+  switch (operation) {
+    case "+":
+      computation = prev + current;
+      break;
+    case "-":
+      computation = prev - current;
+      break;
+    case "÷":
+      computation = prev / current;
+      break;
+    case "×":
+      computation = prev * current;
+      break;
+  }
+
+  return computation.toString();
+}
+
+const INTEGER_FORMATTER = new Intl.NumberFormat("en-us", {
+  maximumFractionDigits: 0,
+});
+
+function formatOperand(operand: any) {
+  if (operand == null) return;
+
+  const [integer, decimal] = operand.split(".");
+
+  if (decimal == null) return INTEGER_FORMATTER.format(integer);
+
+  return `${INTEGER_FORMATTER.format(integer)}.${decimal}`;
+}
+
 function App() {
   const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(
     reducer,
-    {}
+    { currentOperand: "0" }
   );
 
   console.log(currentOperand);
@@ -95,9 +157,9 @@ function App() {
       <div className="grid">
         <div className="dis">
           <div className="previous-operand">
-            {previousOperand} {operation}
+            {formatOperand(previousOperand)} {operation}
           </div>
-          <div className="current-operand">{currentOperand}</div>
+          <div className="current-operand">{formatOperand(currentOperand)}</div>
         </div>
 
         <div
@@ -125,7 +187,7 @@ function App() {
         <DigitButton digit="2" dispatch={dispatch} />
         <DigitButton digit="3" dispatch={dispatch} />
         <OperationButton operation="+" dispatch={dispatch} />
-        <div className="padButton dot">.</div>
+        <DigitButton digit="." dispatch={dispatch} />
         <DigitButton digit="0" dispatch={dispatch} />
         <div
           onClick={() => dispatch({ type: ACTIONS.EVALUATE })}
